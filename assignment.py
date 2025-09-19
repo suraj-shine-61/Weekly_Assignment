192811aaa836d7b4a18755bc3682976f11301050
su-25063-hub <su-25063@sitare.org>
2025-09-19 23:02:41 +0530

    add

--- assignment.py
+++ assignment.py
@@ -1,2 +1,6 @@
-print('hello')
-print('hi')
+list=[1,2,3,3,1,2,4,2,4]
+unique_element=[]
+for num in list:
+    if num  not in unique_element:
+        unique_element.append(num)
+        print(num)
