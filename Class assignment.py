d16cdd6d6030322d0d156a9bbc31a46382af7b38
su-25063-hub <su-25063@sitare.org>
2025-09-20 11:19:35 +0530

    add

--- Class assignment.py
+++ Class assignment.py
@@ -4,3 +4,13 @@ for num in list:
     if num  not in unique_element:
         unique_element.append(num)
         print(num)
+
+
+words=['my','name','is','suraj','my','is']
+freq={}
+for word in words:
+    if word in freq:
+        freq[word]+=1
+    else:
+        freq[word]=1
+print(freq)
