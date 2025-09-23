9ed95be3f635f657d3fadf98c101f571d363c339
su-25063-hub <su-25063@sitare.org>
2025-09-24 01:02:09 +0530

    add

--- new.py
+++ new.py
@@ -1,25 +1,3 @@
-with open ('demo.txt') as f:
-            r=f.read()
-            print(r)
 
-with open ('demo.txt','w')as f:
-    r=f.write('hi\n')
-
-with open ('demo.txt','a+') as f:
-    r=f.write('from where are you')
-    f.seek(0)
-
-with open('demor.txt','r+') as f :
-    r=f.write('the file is exclusive creative')
-    
-    
-    
-with open ('demo.txt','w+') as f :
-    f.write('fine and what about you\n')
-    f.seek(0)
-    
-with open('demo.txt','a+') as f:
-    f.write('where are you going?')
-    
-with open('demo.txt','a+') as f:
-    f.write('but now I am fine.')
+with open('demo.txt','r') as f:
+    f.write('I think you all are fine/nBy today onwards u feel bad')
